d5962953b6a5baf7fc1fe86f5bf8e1af1dd9cb67
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-03-29 12:38:56 -0600

    clona tabla

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,26 @@
 function agregarTarea(){
   var nuevoElemento = document.getElementById("nuevoElemento").value;
   var node = document.createElement("li");
-  var textNode = document.createTextNode(nuevoElemento);
   var llamar = document.getElementById("lista");
   var lista = llamar.appendChild(node);
   lista.innerHTML = nuevoElemento;
   document.getElementById("nuevoElemento").value = "";
 }
+
+// function agregarTitulo(){
+//   var titulo = document.getElementById("agregartitulo");
+//
+// }
+
+function agregarLista(){
+  var nuevaTab = document.getElementById("nuevaTab");
+  var clon = nuevaTab.cloneNode(true);
+  document.getElementById("nuevaSeccion").appendChild(clon);
+  agregarTarea();
+}
+
+
+// var node = document.createElement("table");
+// var nuevaTab = document.getElementById("nuevaTab");
+// var tabla = nuevaTab.appendChild(node);
+// nuevaTab.innerHTML = nuevaTarea;
